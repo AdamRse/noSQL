6f705bcc50ea5cb3b659fa8abd43d0de0f5058a9
AdamRse <bakaarion3@gmail.com>
2024-06-28 16:50:33 +0200

    oui

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const mustacheExpress = require("mustache-express");
 const path = require("path");
 const db = require('./config/database')
+const cors = require('cors')
 
 //variables de modules
 const app = express();
@@ -15,14 +16,15 @@ app.set("views", __dirname + "/views");
 app.use(express.static("public"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static('public', { maxAge: 0 }));
+app.use(express.static('public', { maxAge: 0 }));0
+app.use(cors());
 
 app.listen(3000, () => {
-    console.log("Server is running on port 3000");
+    console.log(`Server is running on port 3000 (PID: ${process.pid})`);
 });
 
 //routes
-app.use("/annonces", require("./routes/annonces"));
+app.use("/annonces", cors(), require("./routes/annonces"));
 
 
 db();
